d096f1c87dd17f0e528ebc31577d93237a5eb5ec
Lukas Thiemeier <thiemeier@strato.de>
2025-06-11 10:33:17 +0200

    Add basic TLS and mTLS support to the grpc mgmt api
    The grpc management api can now be configured to enforce TLS connections.
    The servers certificate and key have to be provided in two separate files.
    Other ways to configure server certificates are currently not implemented.
    
    The server can require the client to send a client certificate for mTLS.
    A root CA for validating the client certificate has to be provided in a
    dedicated ca file. The client will accept all valid certificates signed
    by the configured client CA.
    
    The Client side TLS config required for the http gateway is currently not
    implemented. If TLS is enabled, the http api gateway will just be
    disabled.
    
    Signed-off-by: Lukas Thiemeier <thiemeier@strato.de>

--- service/standalone/start.go
+++ service/standalone/start.go
@@ -185,12 +185,16 @@ func Start(ctx context.Context, app dom.AppInfo, conf *Config) error {
 
 	_, useFakeStorageCreds := fake[conf.Proxy.Auth.UseStorage]
 
+	httpPort := httpLocalhost(conf.Api.HttpPort)
+	if conf.Api.Secure {
+		httpPort = "disabled due to grpc TLS"
+	}
 	fmt.Printf(connectInfo,
 		uiURL,
 		proxyURL,
 		printCreds(conf, useFakeStorageCreds),
 		localhost(conf.Api.GrpcPort),
-		httpLocalhost(conf.Api.HttpPort),
+		httpPort,
 		redisSvc.Addr(),
 		printStorages(fake, conf.Storage),
 	)
